ff319ca0376c5fba1c7dc02a240314b12afbc8d3
Patrick Mackle <patrickthemackle@gmail.com>
2023-03-09 16:21:20 -0800

    begin implementation of animated letters component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,50 @@
 import LogoTitle from "../../assets/images/logo-p.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate"); // useState for animation on hover
+  const nameArray = ["a", "t", "r", "i", "c", "k"];
+  const jobArray = [
+    "w",
+    "e",
+    "b",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+  ];
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
-          <img src={LogoTitle} alt="developer" />
-          atrick
+          <span className={letterClass}>H</span>
+          {/*This one diff classname, so it animates AFTER previous letter */}
+          <span className={`${letterClass} _12`}>i</span>
           <br />
-          web developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={21}
+          />
         </h1>
         <h2>Frontend Developer / JavaScript Fella / Bread Baker</h2>
         <Link to="/contact" className="flat-button">
