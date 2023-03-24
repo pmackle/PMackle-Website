316a9b6a8f9cf2e01fc67c5151b31b928e6e48b4
Patrick Mackle <patrickthemackle@gmail.com>
2023-03-24 15:34:40 -0700

    added basic contact form

---
+++ src/components/Contact.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "./AnimatedLetters";
+import "./Contact.scss";
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState("text-animate"); // useState for animation on hover
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={"Contact me".split("")}
+              idx={15}
+            />
+          </h1>
+          <p>Placeholder paragraph</p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default Contact;
