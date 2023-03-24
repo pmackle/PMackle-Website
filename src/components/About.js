9f0560731161cd4d5ba8f98c1c900720554a7a53
Patrick Mackle <patrickthemackle@gmail.com>
2023-03-24 14:13:34 -0700

    added fun cube animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,16 @@
+import {
+  faCss3Alt,
+  faGitSquare,
+  faHtml5,
+  faJsSquare,
+  faNodeJs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState, useEffect } from "react";
 import AnimatedLetters from "./AnimatedLetters";
+import "./About.scss";
+import Loader from "react-loaders";
 
 const About = () => {
   const [letterClass, setLetterClass] = useState("text-animate"); // useState for animation on hover
@@ -11,21 +22,47 @@ const About = () => {
   }, []);
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          {/* 15 means 1.5 second delay */}
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={"About me".split("")}
-            idx={15}
-          />
-        </h1>
-        <p>Placeholder paragraph 1</p>
-        <p>Placeholder paragraph 2</p>
-        <p>Placeholder paragraph 3</p>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            {/* 15 means 1.5 second delay */}
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={"About me".split("")}
+              idx={15}
+            />
+          </h1>
+          <p>Placeholder paragraph 1</p>
+          <p>Placeholder paragraph 2</p>
+          <p>Placeholder paragraph 3</p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3Alt} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED5F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faNodeJs} color="#6CC24A" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitSquare} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
