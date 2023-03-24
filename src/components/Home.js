92a495121da7f7625220c6fff8bdd1375c12ef4f
Patrick Mackle <patrickthemackle@gmail.com>
2023-03-24 14:19:11 -0700

    added fun pacman page loader animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import AnimatedLetters from "./AnimatedLetters";
 import "./Home.scss";
+import Loader from "react-loaders";
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState("text-animate"); // useState for animation on hover
@@ -16,34 +17,37 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          {/*This one diff classname, so it animates AFTER previous letter */}
-          <span className={`${letterClass} _12`}>i</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={21}
-          />
-        </h1>
-        <h2>Frontend Developer / Walking Enthusiast / Film Guy</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            {/*This one diff classname, so it animates AFTER previous letter */}
+            <span className={`${letterClass} _12`}>i</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={21}
+            />
+          </h1>
+          <h2>Frontend Developer / Walking Enthusiast / Film Guy</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
